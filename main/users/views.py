609f9a5e7f49b1e9f8290487a96b36403ad2c39b
Arthur Costa <arthurdacosta.br@gmail.com>
2026-08-25 21:30:53 -0300

    feat: add form validation in users app views

--- main/users/views.py
+++ main/users/views.py
@@ -1,49 +1,47 @@
 from django.shortcuts import render, redirect
 from . forms import SignupForm, LoginForm
-from django.contrib.auth import get_user_model, authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 
 
 def signup_view(request):
-    form = {
-        "form": SignupForm(),
-    }
-
     if request.method == "POST":
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = request.POST.get("password2")
+        form = SignupForm(request.POST)
+        
+        if form.is_valid():
+            User = get_user_model()
 
-        User = get_user_model()
+            email = form.cleaned_data.get("email")
 
-        if User.objects.filter(email__iexact=email).exists():
-            return HttpResponse("A user already exists with same email.")
+            if User.objects.filter(email__iexact=email).exists():
+                form.add_error("email", "Email already in use.")
+            else:
+                form.save()
+                return HttpResponse(f"Welcome {form.cleaned_data.get('username')}!")
+        
+        return render(request, 'signup/signup.html', context={"form":form})
 
-        User.objects.create_user(
-            username=username, 
-            email=email, 
-            password=password,
-        )
-
-        return redirect('/account/login/')
-
-    return render(request, 'signup/signup.html', form)
+    else:
+        return render(request, 'signup/signup.html', context={"form":SignupForm()})
+        
 
 def login_view(request):
-    form = {
-        "form": LoginForm()
-    }
-
     if request.method == "POST":
-        email = request.POST.get("email")
-        password = request.POST.get("password")
+        form = LoginForm(request.POST)
 
-        user = authenticate(request, email=email, password=password)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
 
-        if user:
-            login(request, user)
-            return HttpResponse(f"Welcome {user.username}.")
+            user = authenticate(request, email=email, password=password)
+        
+            if user:
+                login(request, user)
+                return HttpResponse("OK")
 
-        return HttpResponse("Invalid credentials.")
+            else:
+                form.add_error(None, "Invalid credentials.")
 
-    return render(request, 'login/login.html', form)
+        return render(request, "login/login.html", context={"form": form})
+
+    return render(request, 'login/login.html', context={"form": LoginForm()})
